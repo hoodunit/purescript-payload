7cb6a21580fc0e6e14cc2c32a66d50e5b278ae67
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2019-06-09 09:45:06 -0400

    Add test for Responser Record

--- test/Response.purs
+++ test/Response.purs
@@ -5,7 +5,7 @@ import Prelude
 import Data.Either (Either(..), note)
 import Data.Map as Map
 import Data.Newtype (unwrap)
-import Payload.Response (RawResponse(..), mkResponse)
+import Payload.Response (RawResponse(..), ResponseBody(..), mkResponse)
 import Test.Unit (TestSuite, suite, test)
 import Test.Unit.Assert as Assert
 
@@ -17,6 +17,9 @@ _header key res = do
 _status :: forall r. RawResponse r -> Either String Int
 _status res = Right $ (unwrap res).status
 
+_body :: forall r. RawResponse r -> Either String (ResponseBody r)
+_body res = Right $ (unwrap res).body
+
 tests :: TestSuite
 tests = suite "Response" do
   suite "Responder" do
@@ -27,3 +30,16 @@ tests = suite "Response" do
       test "sets Content-Type to text/plain" do
         res <- mkResponse "foo"
         Assert.equal (Right "text/plain") (res >>= _header "Content-Type")
+      test "leaves body untouched" do
+        res <- mkResponse "foo"
+        Assert.equal (Right (StringBody "foo")) (res >>= _body)
+    suite "record" do
+      test "sets status to 200" do
+        res <- mkResponse { id: 1 }
+        Assert.equal (Right 200) (res >>= _status)
+      test "sets Content-Type to application/json" do
+        res <- mkResponse { id: 1 }
+        Assert.equal (Right "application/json") (res >>= _header "Content-Type")
+      test "encodes body" do
+        res <- mkResponse { id: 1 }
+        Assert.equal (Right (StringBody "{\"id\":1}")) (res >>= _body)
