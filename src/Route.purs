064b757ea8f3514e42932f235a8700e603bceeec
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2019-09-25 19:29:58 -0400

    Remove unused route data types

--- src/Route.purs
+++ src/Route.purs
@@ -1,16 +1,8 @@
 module Payload.Route where
 
-import Data.Symbol
-import Prelude
-
 import Payload.GuardParsing (GNil, Guards(..))
 
 data Route (m :: Symbol) (p :: Symbol) spec = Route
-data GET_ (m :: Symbol) spec = GET_
-data HEAD_ (m :: Symbol) spec = HEAD_
-data POST_ (m :: Symbol) spec = POST_
-data PUT_ (m :: Symbol) spec = PUT_
-data DELETE_ (m :: Symbol) spec = DELETE_
 type GET = Route "GET"
 type HEAD = Route "HEAD"
 type POST = Route "POST"
