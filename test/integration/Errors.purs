45df5386512be94b2e57db849691da18b16bb767
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2019-10-06 22:01:39 -0400

    Use error helper to slightly simplify error test handler

--- test/integration/Errors.purs
+++ test/integration/Errors.purs
@@ -5,7 +5,8 @@ import Prelude
 import Data.Either (Either(..))
 import Data.Map as Map
 import Payload.Headers as Headers
-import Payload.Response (RawResponse, Response(..), ResponseBody(..))
+import Payload.Response (Failure, RawResponse, Response(..), ResponseBody(..))
+import Payload.Response as Response
 import Payload.Spec (GET)
 import Payload.Status as Status
 import Payload.Test.Helpers (respMatches, withRoutes)
@@ -26,7 +27,7 @@ tests = do
         respMatches { status: 500, body: "Error" } res
     test "to return an arbitrary error response, return Either ServerError" $ do
       let spec = Proxy :: _ { foo :: GET "/foo" { response :: String } }
-      let handlers = { foo: \_ -> pure (Left (Response { status: Status.badRequest, headers: Headers.empty, body: StringBody "Error!" }) :: Either RawResponse String) }
+      let handlers = { foo: \_ -> pure (Left (Response.serverError Status.badRequest "Error!") :: Either Failure String) }
       withRoutes spec handlers do
         res <- get "/foo"
         Assert.equal { status: 400, body: "Error!", headers: Map.empty } res
