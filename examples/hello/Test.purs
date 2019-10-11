7b9e729ca3927abc87ddb2f90b6d3e0198d85dc7
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2019-10-11 19:06:24 -0400

    Start hello example server in same way as for other tests

--- examples/hello/Test.purs
+++ examples/hello/Test.purs
@@ -2,26 +2,23 @@ module Payload.Examples.Hello.Test where
 
 import Prelude
 
-import Effect.Class (liftEffect)
-import Payload.Examples.Hello.Main (api, main, spec)
-import Payload.Server as Payload
-import Payload.Test.Helpers (loggingErrors, respMatches, withServer)
+import Payload.Examples.Hello.Main (api, spec)
+import Payload.Test.Helpers (respMatches, withRoutes)
 import Payload.Test.Helpers as Helpers
 import Test.Unit (TestSuite, suite, test)
 import Test.Unit.Assert as Assert
 
 tests :: TestSuite
 tests = do
+  let withApi = withRoutes spec api
   suite "Hello world" do
     let { get } = Helpers.request "http://localhost:3000"
-    test "GET /user/12/messages?limit=2 succeeds" $ do
-      loggingErrors (Payload.start_ spec api) do
-        res <- get "/users/12/messages?limit=2"
-        respMatches
-          { status: 200
-          , body: "[{\"text\":\"Hey there\",\"id\":1},{\"text\":\"Limit 2\",\"id\":2}]" }
-          res
-    test "GET /user/12/messages?limit=2.5 fails with 404 Not Found" $ do
-      loggingErrors (Payload.start_ spec api) do
-        res <- get "/users/12/messages?limit=2.5"
-        Assert.equal 404 res.status
+    test "GET /user/12/messages?limit=2 succeeds" $ withApi do
+      res <- get "/users/12/messages?limit=2"
+      respMatches
+        { status: 200
+        , body: "[{\"text\":\"Hey there\",\"id\":1},{\"text\":\"Limit 2\",\"id\":2}]" }
+        res
+    test "GET /user/12/messages?limit=2.5 fails with 404 Not Found" $ withApi do
+      res <- get "/users/12/messages?limit=2.5"
+      Assert.equal 404 res.status
