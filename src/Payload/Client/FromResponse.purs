107f2fe8f9a246214b4e114d5c1d2ff2b8a32f27
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2019-10-29 19:37:37 -0400

    Show compile-time warning if client cannot query all API endpoints

--- src/Payload/Client/FromResponse.purs
+++ src/Payload/Client/FromResponse.purs
@@ -5,7 +5,9 @@ import Prelude
 import Data.Bifunctor (lmap)
 import Data.Either (Either(..))
 import Node.Stream as Stream
+import Payload.Internal.TypeErrors (type (<>), type (|>))
 import Payload.Response (ResponseBody(..))
+import Prim.TypeError (class Warn, Quote, Text)
 import Simple.JSON as SimpleJson
 import Type.Equality (class TypeEquals)
 import Unsafe.Coerce (unsafeCoerce)
@@ -33,5 +35,12 @@ else instance fromResponseArray ::
   fromResponse _ = Left "Invalid response type, expected String"
 -- | Adding a default instance allows the client to be incomplete:
 -- | not all responses are supported.
-else instance fromResponseDefault :: FromResponse any where
+else instance fromResponseDefault ::
+  Warn (Text "API client cannot query all of endpoints in API spec:"
+          |> Text ""
+          |> Text "No type class instance was found for"
+          |> Text ""
+          |> Text "FromResponse " <> Quote a
+          |> Text "")
+  => FromResponse a where
   fromResponse _ = Left "Could not decode response - no FromResponse instance"
