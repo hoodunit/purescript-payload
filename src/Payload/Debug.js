4d750815762886a304db1b2da82379d0183d74ca
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2020-01-09 09:44:50 +0200

    Add somewhat hacky way of doing debug logging of requests

---
+++ src/Payload/Debug.js
@@ -0,0 +1,7 @@
+exports.showDebugRecordImpl = function(r) {
+  return JSON.stringify(r, null, 2)
+}
+
+exports.showDebugArrayImpl = function(r) {
+  return JSON.stringify(r, null, 2)
+}
---
+++ src/Payload/Debug.purs
@@ -0,0 +1,35 @@
+module Payload.Debug where
+
+import Prelude
+
+import Data.Either (Either(..))
+import Data.String as String
+import Data.Tuple (Tuple(..))
+import Payload.Headers (Headers, toUnfoldable)
+import Payload.ResponseTypes (Response(..))
+
+foreign import showDebugRecordImpl :: forall a. Record a -> String
+foreign import showDebugArrayImpl :: forall a. Array a -> String
+
+class ShowDebug a where
+  showDebug :: a -> String
+
+instance showDebugResponse :: ShowDebug a => ShowDebug (Response a) where
+  showDebug (Response r) =
+    "Status: " <> show r.status.code <> " " <> r.status.reason <> "\n" <>
+    "Headers:\n" <> showHeaders r.headers <> "\n" <>
+    "Body:\n" <> showDebug r.body
+    where
+      showHeaders :: Headers -> String
+      showHeaders headers = "  " <> String.joinWith "\n  " (showHeader <$> toUnfoldable headers)
+      showHeader :: Tuple String String -> String
+      showHeader (Tuple key val) = show key <> " " <> show val
+else instance showDebugEither :: (ShowDebug a, ShowDebug b) => ShowDebug (Either a b) where
+  showDebug (Right a) = "(Right " <> showDebug a <> ")"
+  showDebug (Left a) = "(Left " <> showDebug a <> ")"
+else instance showDebugArray :: ShowDebug (Array a) where
+  showDebug r = showDebugArrayImpl r
+else instance showDebugRecord :: ShowDebug (Record a) where
+  showDebug r = showDebugRecordImpl r
+else instance showDebugDefault :: Show a => ShowDebug a where
+  showDebug = show
